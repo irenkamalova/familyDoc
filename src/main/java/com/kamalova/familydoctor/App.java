9c264ecd5a80e51b1d6d21e5bbcb687d7882d44e
irenkamalova <irenkamalova@yandex-team.ru>
2019-03-09 13:27:30 +0300

    init

---
+++ src/main/java/com/kamalova/familydoctor/App.java
@@ -0,0 +1,87 @@
+package com.kamalova.familydoctor;
+
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.quartz.impl.StdSchedulerFactory;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class App {
+
+    public static void main(String[] args) {
+        try (InputStream secretIn = App.class.getClassLoader()
+                .getResourceAsStream("secrets.properties");
+             InputStream generalIn = App.class.getClassLoader()
+                     .getResourceAsStream("general.properties")) {
+
+            Properties secretProperties = new Properties();
+            secretProperties.load(secretIn);
+            String botToken = secretProperties.getProperty("bot.token");
+
+            Properties generalProperties = new Properties();
+            generalProperties.load(generalIn);
+            String botName = generalProperties.getProperty("bot.name");
+
+
+            ApiContextInitializer.init();
+            TelegramBotsApi botsApi = new TelegramBotsApi();
+            try {
+                FamilyDoctorBot familyDoctorBot = new FamilyDoctorBot(botToken, botName);
+                scheduleBot(familyDoctorBot);
+                botsApi.registerBot(familyDoctorBot);
+                System.out.println("Registered successfully");
+            } catch (TelegramApiException |
+                    SchedulerException e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            System.out.println("Failed to read properties");
+        }
+    }
+
+    private static void scheduleBot(FamilyDoctorBot bot) throws SchedulerException {
+        Map<String, String> timeMessages = timeMessage();
+
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+
+        scheduler.start();
+        for (Map.Entry<String, String> entry : timeMessages.entrySet()) {
+            JobDetail jobDetail = newJob(FDJob.class).build();
+            CronScheduleBuilder schedule = CronScheduleBuilder.cronSchedule(entry.getKey());
+            jobDetail.getJobDataMap().putIfAbsent("FamilyDoctorBot", bot);
+            jobDetail.getJobDataMap().putIfAbsent("Message", entry.getValue());
+
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(schedule)
+                    .build();
+
+            scheduler.scheduleJob(jobDetail, trigger);
+        }
+    }
+
+    private static Map<String, String> timeMessage() {
+        Map<String, String> map = new HashMap<>();
+        map.put("0 15 8 ? * MON-FRI *", "");
+        map.put("0 0 10 ? * MON-FRI *", "");
+        map.put("0 0 11 ? * MON-FRI *", "");
+        map.put("0 0 12 ? * MON-FRI *", "");
+        map.put("0 0 16 ? * MON-FRI *", "");
+        map.put("0 0 18 ? * MON-FRI *", "");
+        map.put("0 0 21 ? * MON-FRI *", "");
+        return map;
+    }
+}
---
+++ src/main/java/com/kamalova/familydoctor/FamilyDoctorBot.java
@@ -0,0 +1,85 @@
+package com.kamalova.familydoctor;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.HashSet;
+import java.util.Set;
+
+import static com.google.common.io.Files.append;
+import static com.google.common.io.Files.readLines;
+
+public class FamilyDoctorBot extends TelegramLongPollingBot {
+
+    private final String botToken;
+    private final String botUserName;
+    private final Set<String> recipientList = new HashSet<>();
+    private final File chatIds = new File("idsBackUp.data");
+
+    FamilyDoctorBot(String botToken, String botUserName) {
+        this.botToken = botToken;
+        this.botUserName = botUserName;
+        initData();
+    }
+
+    private void initData() {
+        try {
+            recipientList.addAll(readLines(chatIds, Charset.defaultCharset()));
+        } catch (IOException e) {
+            System.out.println("Failed to init data");
+            e.printStackTrace();
+        }
+    }
+
+    private void putData(Long chatId) {
+        String strChatId = String.valueOf(chatId);
+        if (recipientList.add(strChatId)) {
+            try {
+                append(strChatId + "\n", chatIds, Charset.defaultCharset());
+            } catch (IOException e) {
+                System.out.println("Failed to append chatId " + strChatId);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    Set<String> getRecipientList() {
+        return recipientList;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            processMessage(update.getMessage());
+        }
+    }
+
+    private void processMessage(Message message) {
+        Long chatId = message.getChatId();
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(chatId);
+        putData(chatId);
+        sendMessage.setText("Здесь будут напоминания");
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botUserName;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+}
